2dd5b50e90a94a73381689eb181ebf03d9753e08
Richard <rcezea007@gmail.com>
2025-09-16 13:05:19 +0100

    done

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -10,19 +10,19 @@ def do_math(operation, a, b):
 
     match operation:
         case '+':
-            print(result.format(a + b))
+            return a + b
 
         case '-':
-            print(result.format(a - b))
+            return a - b
 
         case '*':
-            print(result.format(a * b))
+            return a * b
 
         case '/':
             if num2 == 0:
                 print("Cannot divide by zero.")
             else:
-                print(result.format(a / b))
+                return a / b
 
 
-do_math(op, num1, num2)
+print('The result is {}.'.format(do_math(op, num1, num2)))
