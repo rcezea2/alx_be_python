5ba89c3a6a659ae0d4f4b91ce346f00989b3f9d8
Richard <rcezea007@gmail.com>
2025-09-21 22:53:55 +0100

    done

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,10 +4,25 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-match time_bound:
-    case 'yes':
-        print('Reminder: {} is a {} priority task that'
-              ' requires immediate attention today!'.format(task, priority))
-    case 'no':
-        print('Note: {} is a {} priority task. Consider'
-              ' completing it when you have free time.'.format(task, priority))
+match priority:
+    case 'high':
+        if time_bound == 'yes':
+            print('Reminder: {} is a {} priority task that'
+                  ' requires immediate attention today!'.format(task, priority))
+        elif time_bound == 'no':
+            print('Note: {} is a {} priority task. Consider'
+                  ' completing it when you have free time.'.format(task, priority))
+    case 'medium':
+        if time_bound == 'yes':
+            print('Reminder: {} is a {} priority task that'
+                  ' requires immediate attention today!'.format(task, priority))
+        elif time_bound == 'no':
+            print('Note: {} is a {} priority task. Consider'
+                  ' completing it when you have free time.'.format(task, priority))
+    case 'low':
+        if time_bound == 'yes':
+            print('Reminder: {} is a {} priority task that'
+                  ' requires immediate attention today!'.format(task, priority))
+        elif time_bound == 'no':
+            print('Note: {} is a {} priority task. Consider'
+                  ' completing it when you have free time.'.format(task, priority))
