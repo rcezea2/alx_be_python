8cdb8f194d4438bb07c6e5683e9a3f7505e52e26
Richard <rcezea007@gmail.com>
2025-10-05 20:42:40 +0100

    done

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    def __init__(self):
+        self.__books = []
+        self.__checked_out = []
+
+    def add_book(self, book: Book):
+        self.__books.append(book)
+
+    def check_out_book(self, title):
+        for book in self.__books:
+            if book.title == title:
+                idx = self.__books.index(book)
+                self.__checked_out.append(book)
+                self.__books.pop(idx)
+                break
+        else:
+            print(f"{title} is not available")
+
+    def return_book(self, title):
+        for book in self.__checked_out:
+            if book.title == title:
+                idx = self.__checked_out.index(book)
+                self.__books.append(book)
+                self.__checked_out.pop(idx)
+                break
+
+
+    def list_available_books(self):
+        for book in self.__books:
+            print(book)
